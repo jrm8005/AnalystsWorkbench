59090fc0bd0c19eb1556f2df481d69ab74985068
J.R. Mauro <jrm8005@gmail.com>
2009-08-01 19:12:52 -0400

    Added 2-column matrix detection code to lexer.c -- inactive and needs to be integrated

--- lexer.c
+++ lexer.c
@@ -49,3 +49,51 @@ input_type lex()
 		return MATRIX;
 	else return VECTOR;
 }
+
+/* TODO: Code for detecting special case 2-column matrix. To be integrated later. */
+#if 0 /* I know rob would kill me for this */
+	while (c = getc(stdin)) {
+		if (nlines >= 2 || c == EOF)
+			break;
+		
+		if (c == '\n') {
+			printf("\n");
+			ndigits=0; 
+			++nlines;
+			if ( c = getc(stdin) != EOF && nlines < 2) {
+				ungetc(c, stdin);
+				i = 1;
+			}
+		}
+		
+		if (isdigit(c) || c == '.') {
+			++ndigits;
+		}
+
+		
+		if (isspace(c)) {
+			ndigits=0;
+			++i;
+		}	
+		printf("i is: %d\n", i);
+
+	}
+	
+	if (chatty) {
+		printf("nlines: %d, i: %d\n", nlines, i);
+	}
+	
+	if (nlines == 1) {
+		if (i < 2)
+			printf("scalar\n");
+		else printf("vector\n");
+		return;
+	}
+	
+	if (i >= 1) {
+		if (i ==2) {
+			printf("2 column matrix\n");
+		} else printf("matrix\n");
+	}
+	else printf("vector\n");
+#endif
